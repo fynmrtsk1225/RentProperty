c00a35d52c5cf92ed823d934d84323785825cd9a
fuyuno moritsuka <fynmrtsk.1225@gmail.com>
2022-06-25 02:27:33 +0900

    物件登録機能を追加

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,15 +1,21 @@
 class PropertiesController < ApplicationController
 
   def index
-    @properties = Properties.all.order("id DESC")
+    @properties = Property.all.order("id DESC")
   end
   
   def new
     @property = Property.new
+    render :index and return if params[:back]
   end
   
   def create
-    
+    @property = Property.build(property_params)
+    if @property.save
+      redirect_to properties_path, notice: "物件を登録しました"
+    else
+      render :new
+    end
   end
 
   def show
@@ -27,6 +33,12 @@ class PropertiesController < ApplicationController
 
   def destroy
     
+  end
+  
+  private
+
+  def property_params
+    params.require(:property).permit(%i[name price address age remarks])
   end
   
 end
